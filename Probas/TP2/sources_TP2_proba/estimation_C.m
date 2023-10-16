76b73c66f1c807ca6037ffbf0d5e80d67cc1c33a
Louis THEVENET <louis.tvnt@gmail.com>
2023-10-16 16:14:08 +0200

    Probas : update TP2

--- Probas/TP2/sources_TP2_proba/estimation_C.m
+++ Probas/TP2/sources_TP2_proba/estimation_C.m
@@ -1,15 +1,11 @@
 % Fonction estimation_C (exercice_2.m)
 
 function C_estime = estimation_C(x_donnees_bruitees,y_donnees_bruitees,tirages_C,R_moyen)
-    distances = tirages_C(:, 1);
-    for i = 1:size(tirages_C, 1)-1
-        tmp = sqrt(...
-            (x_donnees_bruitees-tirages_C(1, i)).*(x_donnees_bruitees-tirages_C(1, i)) ...
-            + (y_donnees_bruitees -tirages_C( 2, i)).*(y_donnees_bruitees-distances(2, i))) 
-            - R_moyen;
-        distances(i, 1) =  tmp' * tmp;
-    end
-    [minimum, index]=min(distances);
-    C_estime=tirages_C(:, index);
-
+    distance_x_carre = (tirages_C(1, :) - x_donnees_bruitees).^2;    
+    distance_y_carre = (tirages_C(2, :) - y_donnees_bruitees).^2;    
+    distance=sqrt(distance_x_carre+distance_y_carre);
+    V=(distance-R_moyen).^2;
+    somme=ones(1, size(V,1))*V;
+    [~, Ind_min]=min(somme);
+    C_estime=tirages_C(:, Ind_min);
 end

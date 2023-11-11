0a6a4cb5bed7583c8cdf86644fe9e6f7a23e115e
Louis THEVENET <louis.tvnt@gmail.Com>
2023-11-11 19:19:00 +0100

    PIM PR02 : Removes unused import

--- PIM/PR02/evaluer_alea_th.adb
+++ PIM/PR02/evaluer_alea_th.adb
@@ -1,7 +1,6 @@
 with Ada.Text_IO;          use Ada.Text_IO;
 with Ada.Integer_Text_IO;  use Ada.Integer_Text_IO;
 with Ada.Command_Line;     use Ada.Command_Line;
-with SDA_Exceptions;       use SDA_Exceptions;
 with Alea;
 with TH;
 

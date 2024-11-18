ad8337343445def1dda36a30f498afed6b553abb
Louis Thevenet <louis.tvnt@gmail.com>
2024-11-18 16:41:08 +0100

    cours(pf2): td1

--- Cours/S7/PF2/td.typ
+++ Cours/S7/PF2/td.typ
@@ -42,3 +42,48 @@ let rec split : 'a. ('a * 'a) perfect_tree -> 'a perfect_tree * 'a perfect_tree
       (Node (l1, t1), Node (l2, t2))
 ```)
 
+= TD6
+$
+  "fold_right": (alpha -> beta -> beta) -> alpha "list" -> beta -> beta
+  &equiv ((alpha times beta)->beta) -> (
+    "unit" -> beta
+  ) -> alpha "list" -> beta \
+  &equiv ((alpha times beta ) "option" -> beta )-> alpha "list" -> beta
+$
+
+$
+  "unfold": (
+    underparen(beta,"type générateur") -> (
+      alpha times underparen(beta, "pour la prochaine génération")
+    ) "option"
+  ) -> (beta -> alpha -> alpha "flux")
+$
+#sourcecode(```ocaml
+module type Iter =
+sig
+type 'a t
+val vide : 'a t
+val cons : 'a -> 'a t -> 'a t
+val uncons : 'a t -> ('a * 'a t) option
+val apply : ('a -> 'b) t -> ('a t -> 'b t)
+val unfold : ('b -> ('a * 'b) option) -> ('b -> 'a t)
+val filter : ('a -> bool) -> 'a t -> 'a t
+val append : 'a t -> 'a t -> 'a t
+end
+```)
+
+
+#sourcecode(```ocaml
+let flux_nul = Flux.unfold (fun ()->Some(0, ())) ()
+(* le flux qui contient tous les entiers relatifs pairs, par ordre croissant en valeur absolue *)
+let flux_pair = Iter.unfold (fun i -> Some(2*i, if i <=0 then 1-i else -i))
+```)
+
+
+== Exercice 1
+#sourcecode(```ocaml
+
+let constant e = Iter.unfold (fun () -> Some(e, ())) ()
+let map f fl = Flux.(apply (constant f) fl)
+let map2 f fl fl' = Flux.(apply (map f fl) fl')
+```)
--- Cours/S7/PF2/test.ml
+++ Cours/S7/PF2/test.ml
@@ -1,20 +1,123 @@
-type zero = Zero
-type 'n succ = Succ of 'n
+module type Iter = sig
+  type 'a t
 
-let rec print :  zero -> unit =
-  fun (Zero :  zero) ->
-    Printf.printf "Zero\n"
+  val vide : 'a t
+  val cons : 'a -> 'a t -> 'a t
+  val uncons : 'a t -> ('a * 'a t) option
+  val apply : ('a -> 'b) t -> 'a t -> 'b t
+  val unfold : ('b -> ('a * 'b) option) -> 'b -> 'a t
+  val filter : ('a -> bool) -> 'a t -> 'a t
+  val append : 'a t -> 'a t -> 'a t
+end
 
-let rec print_succ :
-  ('a -> string) ->
-  'b succ -> unit
-= fun f ->
-  function
-  | Succ x ->
-      let s = f x in
-      Printf.printf "Succ %s" s;
-      print Succ
+type 'a t = Tick of (unit -> ('a * 'a t) option)
 
-(* Example usage:
-print (Zero : int zero);;
-print_succ (fun n -> string_of_int n) (Succ 3);;
+module Flux = struct
+  let vide = Tick (fun () -> None)
+  let cons t q = Tick (fun () -> Some (t, q))
+  let uncons (Tick fflux) = fflux ()
+
+  let rec unfold f e =
+    Tick
+      (fun () ->
+        match f e with None -> None | Some (t, e') -> Some (t, unfold f e'))
+
+  let rec apply f x =
+    Tick
+      (fun () ->
+        match (uncons f, uncons x) with
+        | None, _ -> None
+        | _, None -> None
+        | Some (tf, qf), Some (tx, qx) -> Some (tf tx, apply qf qx))
+
+  let rec filter p flux =
+    Tick
+      (fun () ->
+        match uncons flux with
+        | None -> None
+        | Some (t, q) ->
+            if p t then Some (t, filter p q) else uncons (filter p q))
+
+  let rec append flux1 flux2 =
+    Tick
+      (fun () ->
+        match uncons flux1 with
+        | None -> uncons flux2
+        | Some (t1, q1) -> Some (t1, append q1 flux2))
+
+  let constant e = unfold (fun () -> Some (e, ())) ()
+  let map f fl = apply (constant f) fl
+  let map2 f fl fl' = apply (map f fl) fl'
+end
+
+type 'a t = Tick of ('a * 'a t) option Lazy.t
+
+module FluxLazy = struct
+  type 'a t = Tick of ('a * 'a t) option Lazy.t
+
+  let vide = Tick (lazy None)
+  let cons t q = Tick (lazy (Some (t, q)))
+  let uncons (Tick flux) = Lazy.force flux
+
+  (* ou bien par filtrage *)
+  (* Ce lazy là force le calcul car il est dans un filtrage *)
+  let uncons (Tick (lazy flux)) = flux
+
+  let rec apply f x =
+    Tick
+      (lazy
+        (match (uncons f, uncons x) with
+        | None, _ -> None
+        | _, None -> None
+        | Some (tf, qf), Some (tx, qx) -> Some (tf tx, apply qf qx)))
+
+  let rec unfold f e =
+    Tick
+      (lazy
+        (match f e with None -> None | Some (t, e') -> Some (t, unfold f e')))
+
+  let rec filter p flux =
+    Tick
+      (lazy
+        (match uncons flux with
+        | None -> None
+        | Some (t, q) ->
+            if p t then Some (t, filter p q) else uncons (filter p q)))
+
+  let rec append flux1 flux2 =
+    Tick
+      (lazy
+        (match uncons flux1 with
+        | None -> uncons flux2
+        | Some (t1, q1) -> Some (t1, append q1 flux2)))
+
+  let constant e = unfold (fun () -> Some (e, ())) ()
+  let map f fl = apply (constant f) fl
+  let map2 f fl fl' = apply (map f fl) fl'
+end
+
+let fibonacci =
+  FluxLazy.unfold (fun (fn, fn1) -> Some (fn, (fn1, fn + fn1))) (0, 1)
+
+let tail f =
+  match FluxLazy.uncons f with None -> failwith "" | Some (_, t) -> t
+
+(* let rec fibonacci = *)
+(* Tick *)
+(* (lazy *)
+(* (Some (0, Tick (lazy (Some (1, map2 ( + ) fibonacci (tail fibonacci))))))) *)
+
+(* Exercice 3 *)
+let integre dt flux =
+  let rec acc =
+    Tick (lazy (Some (0., FluxLazy.map2 (fun a f -> a +. (f *. dt)) acc flux)))
+  in
+  acc
+
+let integre dt flux =
+  let iter (acc, flux) =
+    match flux with
+    | None -> None
+    | Some (f, q) -> Some (acc, (acc +. (f *. dt), q))
+  in
+  FluxLazy.unfold iter (0., flux)

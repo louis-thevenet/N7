ea87f5a6ad94b3f33a245840e604abb5536de5ad
Louis Thevenet <louis.tvnt@gmail.com>
2025-02-15 08:44:47 +0100

    build: add overlay for ocaml + improve flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "A Nix flake dev environment for N7 assignements (Matlab, Coq, Gnat, X2GO, ...)";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/c407032be28ca2236f45c49cfb2b8b3885294f7f";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
 
@@ -20,7 +22,104 @@
           system,
           ...
         }:
+        let
+          base = with pkgs; [
+            nixd
+            tinymist
+            typstyle
+
+          ];
+          utils = with pkgs; [
+
+            unzip
+            vpnc
+            filezilla
+            x2goclient
+          ];
+          modelisation = with pkgs; [
+            opam # il faut installer les packages why3-coq, why3
+            # et run : eval $(opam env)
+            autoconf
+            pkg-config
+            gtk3
+            gtksourceview
+            coqPackages.coqide
+            coq
+          ];
+          pim = with pkgs; [
+            gnat
+            gprbuild
+            valgrind
+            gdb
+            hyperfine
+          ];
+          c = with pkgs; [
+            python310Packages.jupyter-c-kernel
+            jupyter
+            cmake
+            clang-tools
+          ];
+          apprentissage = with pkgs; [
+
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.pytest
+              python-pkgs.jupyter
+              python-pkgs.numpy
+              python-pkgs.matplotlib
+              python-pkgs.scikit-learn
+              python-pkgs.tensorflow
+              python-pkgs.keras
+              python-pkgs.treelib
+            ]))
+          ];
+          java = with pkgs; [
+            jdk21
+          ];
+          arduino = with pkgs; [
+            # Arduino (needs aditionnal udev rules:
+            # see https://github.com/louis-thevenet/nixos-config/blob/67c87176c875801dd2a65a699189bd9959da4837/hosts/hircine/default.nix#L70C1-L75C6)
+            arduino-core
+            arduino-ide
+          ];
+          ocaml = with pkgs.nixpkgs-ocaml-old; [
+            ocaml
+            dune_2
+            ocamlPackages.utop
+            ocamlPackages.ocamlformat
+            ocamlPackages.menhir
+            ocamlPackages.ppx_inline_test
+            ocamlPackages.ppx_expect
+          ];
+          julia = with pkgs; [
+            julia_19
+          ];
+          ro = with pkgs; [
+            glpk
+          ];
+          sys-transitions = with pkgs; [
+            tlaplus
+            tlaps
+            tlaplusToolbox
+            texliveSmall
+          ];
+          web = with pkgs; [
+            tomcat
+            jdk17
+          ];
+        in
         {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                nixpkgs-ocaml-old = (
+                  import inputs.nixpkgs-old {
+                    system = system;
+                  }
+                );
+              })
+            ];
+          };
           devShells.default = pkgs.mkShell {
             #   # Matlab (needs a working matlab install elsewhere, see https://gitlab.com/doronbehar/nix-matlab)
             #   shellHook = nix-matlab.shellHooksCommon;
@@ -30,91 +129,7 @@
             #     matlab-mex
             #   ];
 
-            packages = with pkgs; [
-              # Nix
-              nil
-              alejandra
-
-              # Typst
-              typst
-              typst-lsp
-              typst-fmt
-
-              # # Modélisation
-              # opam # il faut installer les packages why3-coq, why3
-              # # et run : eval $(opam env)
-              # autoconf
-              # pkg-config
-              # gtk3
-              # gtksourceview
-              # coqPackages.coqide
-              # coq
-
-              # # PIM (Ada)
-              # gnat
-              # gprbuild
-              # valgrind
-              # gdb
-              # hyperfine
-
-              # # C
-              # python310Packages.jupyter-c-kernel
-              # jupyter
-              # cmake
-              # clang-tools
-
-              # # Apprentissage (il manque des modules, je conseille pas de l'utiliser)
-              (pkgs.python3.withPackages (python-pkgs: [
-                python-pkgs.pytest
-                # python-pkgs.jupyter
-                # python-pkgs.numpy
-                # python-pkgs.matplotlib
-                # python-pkgs.scikit-learn
-                # python-pkgs.tensorflow
-                # python-pkgs.keras
-                # python-pkgs.treelib
-              ]))
-
-              # # Arduino (needs aditionnal udev rules:
-              # # see https://github.com/louis-thevenet/nixos-config/blob/67c87176c875801dd2a65a699189bd9959da4837/hosts/hircine/default.nix#L70C1-L75C6)
-              # arduino-core
-              # arduino-ide
-
-              # # Java
-              # jdk23
-
-              # OCaml
-              # ocaml
-              # dune_3
-              # ocamlPackages.utop
-              # ocamlPackages.ocamlformat
-              # ocamlPackages.menhir
-              # ocamlPackages.graphics
-              # ocamlPackages.ppx_inline_test
-              # ocamlPackages.ppx_expect
-
-              # # Julia
-              # julia_19
-
-              # # Recherche opérationnelle
-              # glpk
-
-              # Systèmes de transition
-              tlaplus
-              tlaps
-              tlaplusToolbox
-              texliveSmall
-
-              # Web
-              tomcat
-              jdk17
-
-              # Utilitaires
-              unzip
-              vpnc
-              filezilla
-              x2goclient
-            ];
+            packages = base ++ utils ++ ocaml ++ sys-transitions ++ web;
           };
         };
     };

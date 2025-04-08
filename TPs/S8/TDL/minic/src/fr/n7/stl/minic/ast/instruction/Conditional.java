93b54d243ecb521ad093f9380e31f498c1181ef7
louis thevenet <louis.tvnt@gmail.com>
2025-04-08 09:46:56 +0200

    tp(tdl): tp2

---
+++ TPs/S8/TDL/minic/src/fr/n7/stl/minic/ast/instruction/Conditional.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package fr.n7.stl.minic.ast.instruction;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import fr.n7.stl.minic.ast.Block;
+import fr.n7.stl.minic.ast.SemanticsUndefinedException;
+import fr.n7.stl.minic.ast.expression.Expression;
+import fr.n7.stl.minic.ast.expression.value.BooleanValue;
+import fr.n7.stl.minic.ast.instruction.declaration.FunctionDeclaration;
+import fr.n7.stl.minic.ast.scope.Declaration;
+import fr.n7.stl.minic.ast.scope.HierarchicalScope;
+import fr.n7.stl.minic.ast.scope.SymbolTable;
+import fr.n7.stl.minic.ast.type.AtomicType;
+import fr.n7.stl.minic.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for a conditional
+ * instruction.
+ * 
+ * @author Marc Pantel
+ *
+ */
+public class Conditional implements Instruction {
+
+	protected Expression condition;
+	protected Block thenBranch;
+	protected Block elseBranch;
+
+	public Conditional(Expression _condition, Block _then, Block _else) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = _else;
+	}
+
+	public Conditional(Expression _condition, Block _then) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "if (" + this.condition + " )" + this.thenBranch
+				+ ((this.elseBranch != null) ? (" else " + this.elseBranch) : "");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope
+	 * .Scope)
+	 */
+	@Override
+	public boolean collectAndPartialResolve(HierarchicalScope<Declaration> scope) {
+
+		return this.condition.collectAndPartialResolve(scope)
+				&& this.thenBranch.collectAndPartialResolve(new SymbolTable(scope))
+				&& (this.elseBranch == null || this.elseBranch.collectAndPartialResolve(new SymbolTable(scope)));
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope
+	 * .Scope)
+	 */
+	@Override
+	public boolean collectAndPartialResolve(HierarchicalScope<Declaration> _scope, FunctionDeclaration _container) {
+		throw new SemanticsUndefinedException("Semantics collect is undefined in Conditional.");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope
+	 * .Scope)
+	 */
+	@Override
+	public boolean completeResolve(HierarchicalScope<Declaration> scope) {
+		return this.condition.completeResolve(scope)
+				&& this.thenBranch.completeResolve(new SymbolTable(scope))
+				&& (this.elseBranch == null || this.elseBranch.completeResolve(new SymbolTable(scope)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see fr.n7.stl.block.ast.Instruction#checkType()
+	 */
+	@Override
+	public boolean checkType() {
+		return this.condition.getType().compatibleWith(AtomicType.BooleanType) && this.thenBranch.checkType()
+				&& (this.elseBranch == null || this.elseBranch.checkType());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register,
+	 * int)
+	 */
+	@Override
+	public int allocateMemory(Register _register, int _offset) {
+		throw new SemanticsUndefinedException("Semantics allocateMemory is undefined in Conditional.");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see fr.n7.stl.block.ast.Instruction#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException("Semantics getCode is undefined in Conditional.");
+	}
+
+}

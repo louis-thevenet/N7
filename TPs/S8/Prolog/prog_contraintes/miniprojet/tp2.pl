6129ac77a060b3989f9042792ad9708003a97187
Simon Lécuyer <simlec2002@gmail.com>
2025-04-08 13:01:36 +0200

    wip : Contraintes redondantes

--- TPs/S8/Prolog/prog_contraintes/miniprojet/tp2.pl
+++ TPs/S8/Prolog/prog_contraintes/miniprojet/tp2.pl
@@ -1,5 +1,8 @@
 :- include(libtp2).
  
+%Modèle Basique
+%1)
+
 interval(_, [], _, [], _, []).
 interval(X, [Xl|Xls], Y, [Yl|Yls], T, [Tl|Tls]) :-
     (Xl #>= X + T #\/
@@ -33,3 +36,58 @@ solve(Num, Xs, Ys) :-
  
     fd_labeling(Xs),
     fd_labeling(Ys).
+ 
+    % 2) Appel à printsol pour écrire la solution dans un fichier
+    printsol('tiles.txt', Xs, Ys, Ts).
+
+% Contraintes redondantes
+% 1)
+
+sum_verticals(_, _, _, _, 0). 
+sum_verticals(Xs, Ys, Ts, T, V) :-
+    sum_verticals_aux(Xs, Ys, Ts, V, 0, Sum),
+    Sum #= T,
+    V1 is V - 1,
+    sum_verticals(Xs, Ys, Ts, T, V1).
+
+sum_verticals_aux([], [], [], _, Acc, Acc).
+sum_verticals_aux([Xl|Xls], [Yl|Yls], [Tl|Tls], V, Acc, Sum) :-
+    (Xl #=< V #/\ V #< Xl + Tl) #<=> InRange,
+    NewAcc #= Acc + InRange * Tl,
+    sum_verticals_aux(Xls, Yls, Tls, V, NewAcc, Sum).
+
+sum_horizontals(_, _, _, _, 0).
+sum_horizontals(Xs, Ys, Ts, T, H) :-
+    sum_horizontals_aux(Xs, Ys, Ts, H, 0, Sum),
+    Sum #= T,
+    H1 is H - 1,
+    sum_horizontals(Xs, Ys, Ts, T, H1).
+
+sum_horizontals_aux([], [], [], _, Acc, Acc).
+sum_horizontals_aux([Xl|Xls], [Yl|Yls], [Tl|Tls], H, Acc, Sum) :-
+    (Yl #=< H #/\ H #< Yl + Tl) #<=> InRange,
+    NewAcc #= Acc + InRange * Tl,
+    sum_horizontals_aux(Xls, Yls, Tls, H, NewAcc, Sum).
+
+bettersolve(Num, Xs, Ys) :-
+    data(Num, T, Ts),
+    length(Ts, N),
+    length(Xs, N),
+    length(Ys, N),
+
+    within_bounds(Xs, Ys, Ts, T),
+
+    noverlap(Xs, Ys, Ts),
+
+    % Ajout des contraintes redondantes
+    sum_verticals(Xs, Ys, Ts, T, T),
+    sum_horizontals(Xs, Ys, Ts, T, T),
+
+    fd_labeling(Xs, [backtracks(B)]),
+    fd_labeling(Ys, [backtracks(B)]),
+
+    % 2) Affichage du nombre de backtracks
+    format("Nombre de backtracks : ~d~n", [B]),
+
+    % Appel à printsol pour écrire la solution dans un fichier
+    printsol('tiles.txt', Xs, Ys, Ts).

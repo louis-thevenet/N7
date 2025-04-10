10b84546ec4c73879ca31fa86c121523b43c4a5e
Louis Thevenet <louis.tvnt@gmail.com>
2025-04-10 07:46:11 +0200

    tp(prolog): search strategy

--- TPs/S8/Prolog/prog_contraintes/miniprojet/tp2.pl
+++ TPs/S8/Prolog/prog_contraintes/miniprojet/tp2.pl
@@ -24,7 +24,7 @@ within_bounds([X|Xs], [Y|Ys], [T|Ts], Max) :-
     within_bounds(Xs, Ys, Ts, Max).
  
  
-deprecated_solve(Num, Xs, Ys) :-
+solve_1(Num, Xs, Ys, Bx, By) :-
     data(Num, T, Ts),
     length(Ts, N),
     length(Xs, N),
@@ -34,8 +34,8 @@ deprecated_solve(Num, Xs, Ys) :-
  
     noverlap(Xs, Ys, Ts),
  
-    fd_labeling(Xs),
-    fd_labeling(Ys),
+    fd_labeling(Xs, [backtracks(Bx)]),
+    fd_labeling(Ys, [backtracks(By)]),
  
     % 2) Appel à printsol pour écrire la solution dans un fichier
     printsol('tiles.txt', Xs, Ys, Ts).
@@ -69,22 +69,51 @@ sum_horizontals_aux([Xl|Xls], [Yl|Yls], [Tl|Tls], H, Acc, Sum) :-
     NewAcc #= Acc + InRange * Tl,
     sum_horizontals_aux(Xls, Yls, Tls, H, NewAcc, Sum).
 
-solve(Num, Xs, Ys, B, B2) :-
+solve_2(Num, Xs, Ys, Bx, By) :-
     data(Num, T, Ts),
     length(Ts, N),
     length(Xs, N),
     length(Ys, N),
+
+    within_bounds(Xs, Ys, Ts, T),
+    noverlap(Xs, Ys, Ts),
 
     % Ajout des contraintes redondantes
     T1 is T-1,
     sum_verticals(Xs, Ys, Ts, T, T1),
     sum_horizontals(Xs, Ys, Ts, T, T1 ),
 
-    within_bounds(Xs, Ys, Ts, T),
-    noverlap(Xs, Ys, Ts),
-
-    fd_labeling(Ys, [backtracks(B)]),
-    fd_labeling(Xs, [backtracks(B2)]),
+    fd_labeling(Xs, [backtracks(Bx)]),
+    fd_labeling(Ys, [backtracks(By)]),
 
     % Appel à printsol pour écrire la solution dans un fichier
     printsol('tiles.txt', Xs, Ys, Ts).
+
+% 2)
+% Pour la version sans contraintes redondantes, on constate que le nombre de backtracks sur Xs (Bx) alterne entre 2 et plus rarement 12, et ce sont les backtracks sur les Ys qui varient de 0 à la taille max du carré.
+% Tandis que pour la version avec contraintes redondantes, Xs vaut en moyenne 12 et Ys continue de varier en croissant. 
+
+% L'ajout de ces contraintes permet de mieux réduire l'espace de recherche et de trouver une solution plus rapidement.
+
+% Stratégie de recherche
+% 1)
+
+solve_3(Num, Xs, Ys, B, NbSol) :-
+    data(Num, T, Ts),
+    length(Ts, N),
+    length(Xs, N),
+    length(Ys, N),
+
+    within_bounds(Xs, Ys, Ts, T),
+    noverlap(Xs, Ys, Ts),
+
+    % Ajout des contraintes redondantes
+    T1 is T-1,
+    sum_verticals(Xs, Ys, Ts, T, T1),
+    sum_horizontals(Xs, Ys, Ts, T, T1 ),
+
+    labeling(Xs, Ys, assign, minmin, B, NbSol),
+
+    % Appel à printsol pour écrire la solution dans un fichier
+    printsol('tiles.txt', Xs, Ys, Ts).
+

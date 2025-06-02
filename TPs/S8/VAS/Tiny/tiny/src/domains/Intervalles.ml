d8c1e1c3450068d45096c90294826e3e6a46f078
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-02 12:09:02 +0200

    vas: update intervalles

--- TPs/S8/VAS/Tiny/tiny/src/domains/Intervalles.ml
+++ TPs/S8/VAS/Tiny/tiny/src/domains/Intervalles.ml
@@ -6,31 +6,34 @@
 type t = Bottom | Itv of int option * int option
 
 (* Extension de <= `a Z U {-oo}. *)
-let leq_minf x y =
+let ( <=- ) x y =
   match (x, y) with
   | None, _ -> true (* -oo <= y *)
   | _, None -> false (* x > -oo (x != -oo) *)
   | Some x, Some y -> x <= y
 
-let min_minf x y = if leq_minf x y then x else y
-let max_minf x y = if leq_minf x y then y else x
-
 (* Extension de <= `a Z U {+oo}. *)
-let leq_pinf x y =
+let ( <=+ ) x y =
   match (x, y) with
   | _, None -> true (* x <= +oo *)
   | None, _ -> false (* +oo > y (y != +oo) *)
   | Some x, Some y -> x <= y
 
-let min_pinf x y = if leq_pinf x y then x else y
-let max_pinf x y = if leq_pinf x y then y else x
+let ( ++ ) x y =
+  match (x, y) with _, None | None, _ -> None | Some x, Some y -> Some (x + y)
+
+let ( +- ) = ( ++ )
+let minm x y = if x <=- y then x else y
+let maxm x y = if x <=- y then y else x
+let minp x y = if x <=+ y then x else y
+let maxp x y = if x <=+ y then y else x
 
 let mk_itv o1 o2 =
   match (o1, o2) with
   | None, _ | _, None -> Itv (o1, o2)
   | Some n1, Some n2 -> if n1 > n2 then Bottom else Itv (o1, o2)
 
-(* a printing function (useful for debuging), *)
+(* a printing function (useful for debugging), *)
 let fprint ff = function
   | Bottom -> Format.fprintf ff "Bottom"
   | Itv (None, Some n) -> Format.fprintf ff "(-oo,%d)" n
@@ -42,7 +45,7 @@ let fprint ff = function
 let order x y =
   match (x, y) with
   | Bottom, _ | _, Itv (None, None) -> true
-  | Itv (n, m), Itv (n', m') -> leq_minf n' n && leq_pinf m m'
+  | Itv (a, b), Itv (a', b') -> a' <=- a && b <=+ b'
   | _ -> false
 
 (* and infimums of the lattice. *)
@@ -57,204 +60,56 @@ let bottom = Bottom
 let join x y =
   match (x, y) with
   | Bottom, other | other, Bottom -> other
-  | Itv (n, m), Itv (n', m') -> mk_itv (min_minf n n') (max_pinf m m')
+  | Itv (a, b), Itv (a', b') -> mk_itv (minm a a') (maxp b b')
 
 let meet x y =
   match (x, y) with
-  | Bottom, other | other, Bottom -> Bottom
-  | Itv (n, m), Itv (n', m') -> mk_itv (max_minf n n') (min_pinf m m')
+  | Bottom, _ | _, Bottom -> Bottom
+  | Itv (a, b), Itv (a', b') -> mk_itv (maxm a a') (minp b b')
 
 let widening x y =
+  let ( >=- ) a b = (not (a <=- b)) || a = b in
+  let ( >+ ) a b = not (a <=+ b) in
+  let ( <-- ) a b = not (a >=- b) in
   match (x, y) with
-  | Bottom, v | v, Bottom -> v
-  | Itv (l1, u1), Itv (l2, u2) ->
-      let l =
-        match (l1, l2) with Some a, Some b when a <= b -> Some a | _ -> None
-      in
-      let u =
-        match (u1, u2) with Some a, Some b when a >= b -> Some a | _ -> None
-      in
-      Itv (l, u)
+  | Bottom, other | other, Bottom -> other
+  | Itv (a, b), Itv (c, d) when c >=- a && d <=+ b -> Itv (a, b)
+  | Itv (a, b), Itv (c, d) when c >=- a && d >+ b -> Itv (a, None)
+  | Itv (a, b), Itv (c, d) when c <-- a && d <=+ b -> Itv (None, b)
+  | Itv (a, b), Itv (c, d) when c <-- a && d >+ b -> Itv (None, None)
+  | _ -> top
 
 let sem_itv n1 n2 = if n2 < n1 then Bottom else Itv (Some n1, Some n2)
 
 let sem_plus x y =
   match (x, y) with
+  | Itv (None, None), _ | _, Itv (None, None) -> Itv (None, None)
   | Bottom, _ | _, Bottom -> Bottom
-  | Itv (n, m), Itv (n', m') -> (
-      match ((n, m), (n', m')) with
-      | (None, Some m), (_, Some m') | (_, Some m), (None, Some m') ->
-          Itv (None, Some (m + m'))
-      | (Some n, None), (Some n', _) | (Some n, _), (Some n', None) ->
-          Itv (Some (n + n'), None)
-      | (Some n, Some m), (Some n', Some m') ->
-          Itv (Some (n + n'), Some (m + m'))
-      | _ -> top)
+  | Itv (a, b), Itv (a', b') -> Itv (a +- a', b ++ b')
 
 let sem_minus x y =
   match (x, y) with
-  | Bottom, _ | _, Bottom -> Bottom
-  | Itv (n, m), Itv (n', m') -> (
-      match ((n, m), (n', m')) with
-      | (None, Some m), (_, Some m') -> Itv (None, Some (m - m'))
-      | (Some n, Some m), (None, Some m') -> Itv (Some (n - m'), None)
-      | (Some n, None), (Some n', _) -> Itv (Some (n - n'), None)
-      | (Some n, Some m), (Some n', None) -> Itv (None, Some (n' - m))
-      | (Some n, Some m), (Some n', Some m') ->
-          Itv (Some (n - n'), Some (m - m'))
-      | _ -> top)
-
-(* let sem_times x y = match x, y with
-  | Bottom, _ | _, Bottom -> Bottom
-  | Itv (n1, n2), Itv (m1, m2) ->
-    let all = List.filter_map (fun (a, b) ->
-      match a, b with
-      | Some a, Some b -> Some (a * b)
-      | _ -> None
-    ) [
-      (n1, m1); (n1, m2); (n2, m1); (n2, m2)
-    ] in
-    if all = [] then Itv (None, None)
-    else
-      let min = Some (List.fold_left min max_int all) in *)
-(* let max = Some (List.fold_left max min_int all) in
-      Itv (min, max) *)
-
-let sem_times x y =
-  let reorder x =
-    match x with
-    | Bottom -> Bottom
-    | Itv (n, m) -> Itv (min_minf n m, max_pinf n m)
-  in
-  match (x, y) with
-  | Bottom, _ | _, Bottom -> Bottom
-  | Itv (n, m), Itv (n', m') -> (
-      match ((n, m), (n', m')) with
-      (* [-oo, m] * [n', m'] *)
-      (* [n', m'] * [-oo, m] *)
-
-      (*[-oo, -1] * [1, 3] -> [-oo, -1] *)
-      | ((None, Some m), (Some n', Some m') | (Some n', Some m'), (None, Some m))
-        when n' >= 0 && m < 0 ->
-          Itv (None, Some (m * n'))
-      (*[-oo, 1] * [1, 3] -> [-oo, 3] *)
-      | ((None, Some m), (Some n', Some m') | (Some n', Some m'), (None, Some m))
-        when n' >= 0 ->
-          Itv (None, Some (m * m'))
-      (*[-oo, -1] * [-1, 3] -> [-oo, -3] *)
-      (*CAS DIFFERNET*)
-
-      (*[-oo, 1] * [-2, -1] -> [-oo, -1] *)
-      (*CAS DIFFERNET*)
-
-      (*[-oo, 1] * [-1, 3] -> [-oo, 3] *)
-      (*[-oo, -1] * [-1, -3] -> [-oo, 3] *)
-      | ((None, Some m), (Some n', Some m') | (Some n', Some m'), (None, Some m))
-        when n' < 0 ->
-          Itv (None, Some (m * m'))
-      (* [n, +oo] * [n', m'] *)
-      (* [n', m'] * [n, +oo] *)
-      | ((Some n, None), (Some n', Some m') | (Some n', Some m'), (Some n, None))
-        when m' >= 0 ->
-          Itv (Some (n * n'), None)
-      (* |(Some n, None),(Some n',Some m') | (Some n',Some m'),(Some n, None) when m' < 0 -> Itv(None, ) *)
-      (* [_, m] * [-oo, m'] *)
-      | (_, Some m), (None, Some m') -> Itv (None, Some (m * m'))
-      | (Some n, None), (Some n', _) | (Some n, _), (Some n', None) ->
-          Itv (Some (n * n'), None)
-      | (Some n, Some m), (Some n', Some m') ->
-          Itv (Some (n * n'), Some (m * m'))
-      | _ -> top)
-
-let sem_div x y =
-  match (x, y) with
-  | Bottom, _ | _, Bottom -> Bottom
-  | _, Itv (Some l, Some u) when l <= 0 && u >= 0 ->
-      Bottom (* division par zéro *)
-  | Itv (n1, n2), Itv (m1, m2) ->
-      let all =
-        List.filter_map
-          (fun (a, b) ->
-            match (a, b) with
-            | Some a, Some b when b <> 0 -> Some (a / b)
-            | _ -> None)
-          [ (n1, m1); (n1, m2); (n2, m1); (n2, m2) ]
+  | Bottom, _ | _, Bottom -> bottom
+  | Itv (a, b), Itv (a', b') ->
+      let left =
+        match (a, b') with
+        | None, _ -> None (*a=-oo*)
+        | _, None -> None (*b=+oo*)
+        | Some a, Some b' -> Some (a - b')
       in
-      if all = [] then Itv (None, None)
-      else
-        let min = Some (List.fold_left min max_int all) in
-        let max = Some (List.fold_left max min_int all) in
-        Itv (min, max)
+      let right =
+        match (b, a') with
+        | None, _ -> None (*b=+oo*)
+        | _, None -> None (*a'=-oo*)
+        | Some b, Some a' -> Some (b - a')
+      in
+      Itv (left, right)
 
+let sem_times x y = top
+let sem_div x y = top
 let sem_guard = function t -> t
-
-let ( + ) x y =
-  match (x, y) with
-  | None, None -> None
-  | None, Some y -> Some y
-  | Some x, None -> Some x
-  | Some x, Some y -> Some (x + y)
-
-and ( - ) x y =
-  match (x, y) with
-  | None, None -> None
-  | None, Some y -> Some y
-  | Some x, None -> Some x
-  | Some x, Some y -> Some (x + y)
-
-let backsem_plus x y r =
-  match (x, y) with
-  | Bottom, _ | _, Bottom -> (Bottom, Bottom)
-  | Itv (nx, mx), Itv (ny, my) -> (
-      match r with
-      | Bottom -> (Bottom, Bottom)
-      | Itv (nr, mr) ->
-          ( Itv (min_minf (nr - ny) nx, max_pinf (mr - my) mx),
-            Itv (min_minf (nr - nx) ny, max_pinf (mr - mx) my) ))
-
-let backsem_minus x y r =
-  match (x, y) with
-  | Bottom, _ | _, Bottom -> (Bottom, Bottom)
-  | Itv (nx, mx), Itv (ny, my) -> (
-      match r with
-      | Bottom -> (Bottom, Bottom)
-      | Itv (nr, mr) ->
-          ( Itv (min_minf (nr + ny) nx, max_pinf (mr + my) mx),
-            Itv (min_minf (nr + nx) ny, max_pinf (mr + mx) my) ))
-
-(* let backsem_plus x y r = 
- 
-  match x, y with
-  | Bottom, _ | _, Bottom-> Bottom, Bottom
-  | Itv (nx,mx), Itv (ny, my) -> begin  
-  
-  match r with
-  (* r € [-oo, +oo]*)
-| Bottom -> Bottom,Bottom
-| Itv (nr,mr) -> begin 
-  match nr, mr with
-  | None, None -> x, y
-  (* r € [-oo, mr]*)
-  | None, Some mr -> 
-      Itv (nx, match my with |None->mx |Some(my)-> min (Some (mr-my)) mx ),
-      Itv (ny, match mx with |None->my |Some(mx)-> min (Some (mr-mx)) my
-      )
-  (* r € [nr, +oo]*)
-  | Some nr, None -> 
-    (
-      (*x € [nx, max(mx, max(nr-ny si ny=Some, nx))]*)
-      (*x € [nx, max(mx, max(nr-ny si ny=Some, nx))]*)
-      Itv ((match ny with |None->nx |Some(ny)-> max (Some (nr-ny)) nx ), mx),
-      Itv ((match nx with |None->ny |Some(nx)-> max (Some (nr-nx)) ny), my)
-    )
-  (* r € [nr, mr]*)
-  | Some (nr), Some(mr) ->
-    (
-      Itv ((match ny with |None->nx |Some(ny)-> min (Some (nr-ny)) nx ), match my with |None->mx |Some(my)-> max (Some (mr-my)) mx )),
-      Itv ((match nx with |None->ny |Some(nx)-> min (Some (nr-nx)) ny), match mx with |None->my |Some(mx)-> max (Some (mr-mx)) my
-    )
-end *)
-(* end *)
+let backsem_plus x y r = (x, y)
+let backsem_minus x y r = (x, y)
 let backsem_minus x y r = (x, y)
 let backsem_times x y r = (x, y)
 let backsem_div x y r = (x, y)
